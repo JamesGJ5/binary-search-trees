4d531983394ec5d328de8ec859c4516ced56ab1f
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-15 19:28:00 +0000

    Remove duplicates from array passed to BST constructor

---
+++ tree.js
@@ -0,0 +1,22 @@
+class Tree {
+  constructor(arr) {
+    this.root = this.#buildTree(arr);
+  }
+
+  #buildTree(arr) {
+    this.#removeDuplicates(arr);
+  }
+
+  #removeDuplicates(arr) {
+    const dataUnrepeated = new Set();
+    while (arr.length > 0) {
+      dataUnrepeated.add(arr.pop());
+    }
+    dataUnrepeated.forEach((data) => {
+      dataUnrepeated.delete(data);
+      arr.push(data);
+    });
+  }
+}
+
+const tree1 = new Tree([1, 5, 3, 2, 6, 5, 8, 7, 6, 7, 0]);
