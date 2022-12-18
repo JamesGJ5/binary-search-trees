dc6d1346027ed15617e5de7af103f0d4684f9d21
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-18 15:55:33 +0000

    Final test-run

--- src/index.js
+++ src/index.js
@@ -1,65 +1,22 @@
 import Tree from './modules/tree';
 
-// const tree1 = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-// tree1.prettyPrint();
+const randomIntegerArray = (size, maximum) =>
+  Array.from({ length: size }, () => Math.floor(Math.random() * maximum));
 
-// console.log(tree1.find(4));
-// console.log(tree1.find(21));
-
-// tree1.insert(1000);
-// tree1.insert(-10);
-// tree1.prettyPrint();
-
-// console.log(tree1.levelOrder());
-
-// tree1.preorder();
-// console.log(tree1.preorder());
-
-const tree1Values = [1, 2, 3, 4, 5];
-const tree1 = new Tree(tree1Values);
-tree1.prettyPrint();
-// tree1.preorder();
-// tree1.inorder();
-// console.log(tree1.inorder());
-// console.log(tree1.postorder());
-// for (let i = 0; i < tree1Values.length; i += 1) {
-  // const value = tree1Values[i];
-  // console.log(value);
-//   console.log(typeof value);
-  // const node = tree1.find(value);
-//   console.log(node);
-//   const height = Tree.height(node);
-//   console.log(height);
-  // const depth = tree1.depth(node);
-  // console.log(depth);
-// }
-// console.log(tree1.isBalanced());
-// tree1.insert(1000);
-// tree1.prettyPrint();
-// console.log(tree1.isBalanced());
-// tree1.insert(4.5);
-// tree1.prettyPrint();
-// console.log(tree1.isBalanced());
-// tree1.insert(3.5);
-// tree1.prettyPrint();
-console.log(tree1.isBalanced());
-tree1.insert(10000);
-tree1.prettyPrint();
-console.log(tree1.isBalanced());
-tree1.rebalance();
-tree1.prettyPrint();
-console.log(tree1.isBalanced());
-
-// const tree2 = new Tree([1]);
-// tree2.prettyPrint();
-// tree2.preorder();
-// tree2.inorder();
-// console.log(tree2.inorder());
-// console.log(tree2.postorder());
-
-// const tree3 = new Tree([]);
-// tree3.prettyPrint();
-// tree3.preorder();
-// tree3.inorder();
-// console.log(tree3.inorder());
-// console.log(tree3.postorder());
+const tree = new Tree(randomIntegerArray(40, 100));
+console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
+console.log('Level-order traversal:', tree.levelOrder());
+console.log('Preorder traversal:', tree.preorder());
+console.log('Postorder traversal:', tree.postorder());
+console.log('Inorder traversal:', tree.inorder());
+tree.insert(1000);
+tree.insert(10000);
+tree.insert(100000);
+tree.insert(1000000);
+console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
+tree.rebalance();
+console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
+console.log('Level-order traversal:', tree.levelOrder());
+console.log('Preorder traversal:', tree.preorder());
+console.log('Postorder traversal:', tree.postorder());
+console.log('Inorder traversal:', tree.inorder());
