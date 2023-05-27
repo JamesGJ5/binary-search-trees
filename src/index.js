bd89743b74be1a9c1396c164aea868611a69c875
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-27 12:50:31 +0100

    Print what the tree looks like at the end

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ const randomIntegerArray = (size, maximum) =>
   Array.from({ length: size }, () => Math.floor(Math.random() * maximum));
 
 const tree = new Tree(randomIntegerArray(40, 100));
+
 console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
 console.log('Level-order traversal:', tree.levelOrder());
 console.log('Preorder traversal:', tree.preorder());
 console.log('Postorder traversal:', tree.postorder());
 console.log('Inorder traversal:', tree.inorder());
+
 tree.insert(1000);
 tree.insert(10000);
 tree.insert(100000);
 tree.insert(1000000);
+
 console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
+
 tree.rebalance();
+
 console.log(`It is ${tree.isBalanced()} that the tree is balanced.`);
 console.log('Level-order traversal:', tree.levelOrder());
 console.log('Preorder traversal:', tree.preorder());
 console.log('Postorder traversal:', tree.postorder());
 console.log('Inorder traversal:', tree.inorder());
+
+tree.prettyPrint();
